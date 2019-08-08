fa01307e555d79b44ba891fb2716472d3ff56db2
tendomart <tmkmart@gmail.com>
2019-08-08 16:59:06 +0300

    Added Address Class and Mocha Tests

---
+++ test/address.js
@@ -0,0 +1,24 @@
+const assert = require('chai').assert;
+const A = require('../address');
+
+//let addr =A;
+describe('Mocking Address Book',function(){
+it('Store ',function(){
+
+});
+it('Store Address and Retrieve',function(){
+let addr = A;
+addr.setfname('Rogers');
+assert(addr.getfname('Rogers')==='Rogers');
+});
+
+it('Retrieve Address Information ...',function(){});
+
+it('',function(){});
+
+it('',function(){});
+
+});
+
+
+
